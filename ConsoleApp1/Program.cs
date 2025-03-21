6a73abc6cb487440d64d64c46e8f83b48d943f8a
theatimbas <altheatimbas97@gmail.com>
2025-03-21 09:49:40 +0800

    Update Program.cs

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,52 +1,83 @@
-﻿using System.ComponentModel.Design;
+﻿using System;
 
-string fname, lname, option;
-int age;
-
-
-Console.WriteLine("Welcome!");
-
-
-
-    Console.WriteLine("Enter First Name: ");
-
-    fname = Console.ReadLine();
-    Console.WriteLine("Enter Last Name: ");
-    lname = Console.ReadLine();
-
-    Console.WriteLine("Do you want to Continue?");
-    
-    Console.WriteLine("YES/NO");
-    option = Console.ReadLine().ToUpper();
-
-    switch (option)
+class Program
 {
-    case "YES":
-        Console.WriteLine("Enter Age: ");
-        if (int.TryParse(Console.ReadLine(), out age))
+    public static void Main()
+    {
+        Console.WriteLine("Welcome to Checker E-Library System");
+        Console.WriteLine("-------------------------------------");
+
+        while (true)
         {
-            if (age > 17)
+            string UserOption = ShowMenu();
+
+            switch (UserOption)
             {
-                Console.WriteLine($"{fname} {lname}, you are Qualified!");
+                case "1":
+                    RegisterMember();
+                    break;
+                case "2":
+                    BookGenre();
+                    break;
+                case "3":
+                    Console.WriteLine("Exiting the Library. Thank you for visiting!");
+                    return;
+                default:
+                    Console.WriteLine("Please Choose between number 1-3");
+                    break;
+
+
             }
-            else
+        }
+    }
+
+    static string ShowMenu()
+    {
+        Console.WriteLine("------------------------");
+        Console.WriteLine("MENU: ");
+        Console.WriteLine("[1] Register");
+        Console.WriteLine("[2] Find more Books");
+        Console.WriteLine("[3] Exit");
+        Console.Write("Choose an option (1/2/3): ");
+        return Console.ReadLine();
+    }
+
+    static void RegisterMember()
+    {
+        Console.WriteLine("--------------------------");
+        Console.WriteLine("Register Member");
+        Console.Write("Enter Full Name or Username: ");
+        Console.ReadLine();
+        Console.WriteLine("Enter Your Age: ");
+        int UserAge;
+        if (int.TryParse(Console.ReadLine(), out UserAge))
+        {
+            if (UserAge > 17)
             {
-                Console.WriteLine($"{fname} {lname}, you are Not Qualified!");
+                Console.WriteLine("-----You are now registered successfully!-----");
+            }
+            else if (UserAge <= 17)
+            {
+                Console.WriteLine("You are still Underage");
+                Console.WriteLine("Thank you for trying!");
             }
         }
         else
         {
-            Console.WriteLine("Invalid Age");
+            Console.WriteLine("Invalid Age! Please enter a valid number.");
         }
-        break;
+    }
+    static void BookGenre()
+    {
+        Console.WriteLine("-----------------");
+        Console.WriteLine("List of Recommended Books and Genres");
+        Console.WriteLine("Fantasy");
+        Console.WriteLine("Romance");
+        Console.WriteLine("Mystery");
+        Console.WriteLine("Science Fiction");
 
-
-    case "NO":
-        Console.WriteLine("Thank you!");
-        break;
-
-    default:
-        Console.WriteLine("Invalid");
-        break;
+        Console.WriteLine("-----------------"); 
+    }
 }
 
+                
